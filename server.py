c8f7fc6055207b195aa68559a098eb459e27b777
HalfHeartGuy <richard.h.guo@gmail.com>
2022-01-04 14:04:03 +0100

    server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 print(__name__)
 app = Flask(__name__)
 
@@ -10,7 +10,11 @@ def hello_world():
 @app.route("/test")
 def qwe():
     paragraph = "<p>Hallo Welt</p>"
-    return "Hello guys" + paragraph
+    listVersions = ["Minecraft 1.0.0:18. November 2011","Minecraft Version 1.1: 12. January 2012","Minecraft Version 1.2: 1. March 2012"]
+    server_response = render_template("start.html", gameName = "minecraft",gameVersions = listVersions)
+    return server_response
+
+
 @app.route("/minecraft1_0")
 def minecraft1_0():
     paragraph = "<h1>minecraft 1.0</h1>"
